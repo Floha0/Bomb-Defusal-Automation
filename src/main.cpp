47be20337235a00d6f0c5f9f7980331e9ce671a5
Furkan Gülhan <fgulhan07@gmail.com>
2024-09-25 04:19:16 +0300

    while loop main

--- src/main.cpp
+++ src/main.cpp
@@ -18,57 +18,63 @@ using namespace std;
 
 
 int main() {
+    while (true) {
+        cout << "1: Basic Wire" << endl << "2: Button" << endl << "3: Keypad" << endl;
+        cout << "4: Simon Says" << endl << "5: Who's First" << endl << "6: Memory" << endl;
+        cout << "7: Mors Code" << endl << "8: Complicated Wires" << endl << "9: Wire Sequences" << endl;
+        cout << "10: Maze" << endl << "11: Passwords" << endl << "12: Knobs" << endl ;
+        int gameIndex;
+        cout << "Select a game: ";
+        cin >> gameIndex;
+        cout << endl;
 
-    cout << "1: Basic Wire" << endl << "2: Button" << endl << "3: Keypad" << endl;
-    cout << "4: Simon Says" << endl << "5: Who's First" << endl << "6: Memory" << endl;
-    cout << "7: Mors Code" << endl << "8: Complicated Wires" << endl << "9: Wire Sequences" << endl;
-    cout << "10: Maze" << endl << "11: Passwords" << endl << "12: Knobs" << endl ;
-    int gameIndex;
-    cout << "Select a game: ";
-    cin >> gameIndex;
-    cout << endl;
 
+        switch (gameIndex) {
+            case 1:
+                startBasicWire();
+            break;
+            case 2:
+                startButton();
+            break;
+            case 3:
+                startKeypad();
+            break;
+            case 4:
+                startSimonSays();
+            break;
+            case 5:
+                startWhosFirst();
+            break;
+            case 6:
+                startMemory();
+            break;
+            case 7:
+                startMorsCode();
+            break;
+            case 8:
+                startComplicatedWires();
+            break;
+            case 9:
+                startWireSequences();
+            break;
+            case 10:
+                startMaze();
+            break;
+            case 11:
+                startPasswords();
+            break;
+            case 12:
+                startKnobs();
+            break;
+            default:
+                cout << "Game Not Found!" << endl;
+            break;
+        }
 
-    switch (gameIndex) {
-        case 1:
-            startBasicWire();
-            break;
-        case 2:
-            startButton();
-            break;
-        case 3:
-            startKeypad();
-            break;
-        case 4:
-            startSimonSays();
-            break;
-        case 5:
-            startWhosFirst();
-            break;
-        case 6:
-            startMemory();
-            break;
-        case 7:
-            startMorsCode();
-            break;
-        case 8:
-            startComplicatedWires();
-            break;
-        case 9:
-            startWireSequences();
-            break;
-        case 10:
-            startMaze();
-            break;
-        case 11:
-            startPasswords();
-            break;
-        case 12:
-            startKnobs();
-            break;
-        default:
-            cout << "Game Not Found!" << endl;
-            break;
+        cout << "Press \"y\" to continue: ";
+        string input;
+        cin >> input;
+        if (input != "y") break;
     }
 
     return 0;
